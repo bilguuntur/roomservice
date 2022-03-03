6054fce6f5cc48a4f35f92d42a362f673190061f
Michael110011 <98576659+Michael110011@users.noreply.github.com>
2022-03-03 14:50:09 +0800

    Update roomservice.py

--- roomservice.py
+++ roomservice.py
@@ -5,6 +5,9 @@ a very hard project
 guests = {'306':'Bilguuntur', '307':'Irmuun', '51':'Michael', '309':'Taivan'}
 roomnumber = input('Hello customer would you please enter your room number: ')
 if roomnumber in guests.keys():
-    maindish=input('What would you like to order as a main dish?')
+    name=
+    maindish=input('Hello 'name'! What would you like to order as a main dish?')
 else:
     print('Sorry there are no users with that number. Please stop being stupid.')
+    
+    
