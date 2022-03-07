7c96ea8eeb49ea98c09f59a295a014fbbfdcab08
Michael110011 <98576659+Michael110011@users.noreply.github.com>
2022-03-07 14:36:02 +0800

    Update roomservice.py

--- roomservice.py
+++ roomservice.py
@@ -2,17 +2,19 @@
 This 
 a very hard project
 """
+mainorder = 0
+sideorder = 0
+drinkorder = 0
 mainmenu = ['Spaghetti','Pizza','Burger']
 guests = {'306':'Bilguuntur', '307':'Irmuun', '51':'Michael', '309':'Taivan'}
 roomnumber = input('Hello customer would you please enter your room number: ')
 if roomnumber in guests.keys():
-    name = guests[roomnumber]
-    print (mainmenu)
-    maindish =input (f"Hello {name}! What would you like to order from the list above as a main dish?")
-    if maindish in mainmenu:
-        print('e')
-    else:
+    while mainorder < 1:
+        name = guests[roomnumber]
         print (mainmenu)
-        maindish=input ("Please choose one of the items above: ")
+        maindish =input (f"Hello {name}! What would you like to order from the list above as a main dish?")
+        if maindish in mainmenu:
+            mainorder = 1
+            print('e')
 else:
     print('Sorry there are no users with that number. Please stop being stupid.')
